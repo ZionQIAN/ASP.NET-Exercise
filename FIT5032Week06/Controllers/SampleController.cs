adf9e68e00c5acb7d6086ad0f5cfb45e37862ee2
Ziang Qian <zqia0010@student.monash.edu>
2020-09-11 18:52:32 -0700

    week06 exercise lab commit

---
+++ FIT5032Week06/Controllers/SampleController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032Week06.Models;
+
+namespace FIT5032Week06.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Sample/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sample/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sample/Create
+        /**[HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        **/
+
+        // GET: Sample/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sample/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sample/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sample/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // Post: Sample/Create
+        [HttpPost]
+        public ActionResult Create(FormOneViewModel model) 
+        {
+            try
+            {
+                string FirstName = model.FirstName;
+
+                string LastName = model.LastName;
+
+                ViewBag.FullName = FirstName + " " + LastName;
+
+                return View();
+            }
+            catch 
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ FIT5032Week06/Models/SampleFormViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace FIT5032Week06.Models
+{
+    public class SampleFormViewModels
+    {
+    }
+
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+}
