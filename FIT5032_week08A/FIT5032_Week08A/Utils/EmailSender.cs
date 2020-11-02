528fd86598a397a76676a489d46abe1859c43afb
Ziang Qian <zqia0010@student.monash.edu>
2020-11-01 20:02:21 -0800

    week8 commit

---
+++ FIT5032_week08A/FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿//using SendGrid;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("cronosqianziang@gmail.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
