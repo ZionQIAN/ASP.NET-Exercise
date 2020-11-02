6a443afd0666fe3a524465a46afeebdf917c0088
Ziang Qian <zqia0010@student.monash.edu>
2020-11-01 18:35:49 -0800

    week07 commit

---
+++ FIT5032_week07/SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿--creating table 'Students'
+Create table [dbo].[Students](
+ [Id] int Identity(1,1) not null,
+ [FirstName] nvarchar(max) not null,
+ [LastName] nvarchar(max) Not null,
+ [UserId] nvarchar(max) Not null
+
+ primary key (Id)
+);
+Go
+
+--creating table 'Units'
+Create table [dbo].[Units](
+ [Id] int Identity(1,1) not null,
+ [Name] nvarchar(max) not null,
+ [Description] nvarchar(max) Not null,
+ [StudentId] int not null
+ primary key (Id)
+ Foreign key (studentId) references students(Id)
+);
+Go
